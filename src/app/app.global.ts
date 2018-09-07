6853a66c209ad12153bf7e2a5afa1655018500cf
aminkhavariionic <43031205+aminkhavariionic@users.noreply.github.com>
2018-09-07 10:34:45 +0430

    0

--- src/app/app.global.ts
+++ src/app/app.global.ts
@@ -1,11 +1,11 @@
 export const App: any = {
-  store           : 'Ionstore 2 - Tabs',  // change this with your app name
-  ContactEmail    : 'ionicpremium@gmail.com', // change this with your email contact
-  url             : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // change this with your URL (please use https, recommended)
+  store           : 'AndroidSrc',  // change this with your app name
+  ContactEmail    : 'info@AndroidSrc.ir', // change this with your email contact
+  url             : 'https://AndroidSrc.ir', // change this with your URL (please use https, recommended)
   OneSignalAppID  : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // change this with your onesignal api key
   GCM             : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // Google Project Number for Google Cloud Messaging
-  consumerKey     : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // change this with your Consumer Key from WooCommerce
-  consumerSecret  : 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx', // change this with your Consumer Secret from WooCommerce
+  consumerKey     : 'ck_b47e3310d21c25f597864669ca77ab3ce6f4cf6d', // change this with your Consumer Key from WooCommerce
+  consumerSecret  : 'cs_fbab66e8b8401c913c9f8f3a45650a1d65ef4e5e', // change this with your Consumer Secret from WooCommerce
 
   paypalSandboxClientID: 'AZjyISbp1zmOhZ0o_iAG3W2IGjlz2hvEC-8cGoQ7fXcMFN9afaRuW0X1B1PVSgkSuTQWOKqM9N4NTkOP',
   paypalLiveClientID: '', // get this from paypal developer dashboard
